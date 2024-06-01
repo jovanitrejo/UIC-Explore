b86b2509d4bf067e47364ab54dd6e71e6bfb9e73
Jovani Trejo <jovanitrejo@icloud.com>
2024-06-01 13:19:46 -0500

    Initial Commit

---
+++ UIC Explore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIC Explore
+//
+//  Created by Jovani Trejo on 6/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIC Explore/UIC_ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIC_ExploreApp.swift
+//  UIC Explore
+//
+//  Created by Jovani Trejo on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct UIC_ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
