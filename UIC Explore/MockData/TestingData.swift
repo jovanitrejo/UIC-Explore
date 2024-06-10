2c099df1144e07c99c7708e51ae3dfebc0d9110c
Jovani Trejo <jovanitrejo@icloud.com>
2024-06-10 09:01:37 -0500

    Added more testing data to discover bugs

--- UIC Explore/MockData/TestingData.swift
+++ UIC Explore/MockData/TestingData.swift
@@ -9,7 +9,8 @@ import Foundation
 import MapKit
 
 let testPlacesInBuilding = [Place(name: "Starbucks", description: "A Coffee Shop", buildingID: "way/664275388", image: "arcstarbucks.jpg", type: "Food"),
-                            Place(name: "Dunkin", description: "Another Coffeeshop", buildingID: "way/664275388", image: "scedunkin.jpeg", type: "Food")
+                            Place(name: "Dunkin", description: "Another Coffeeshop", buildingID: "way/664275388", image: "scedunkin.jpeg", type: "Food"),
+                            Place(name: "Disability Cultural Center", description: "blah blah blah", buildingID: "way/151557439", image: "dccuic.jpg", type: "cultural center")
                             ]
 let testBuildingAsDict = ["way/664275388": Building(id: "way/664275388", name: "Academic and Residential Complex", address: "940 West Harrison St", coordinates: CLLocationCoordinate2D(latitude: 41.8747701, longitude: -87.6511207), type: "Mixed-Use", image: "arcimage.jpg", description: "Offering private room or shared (2-person) bedrooms in traditional and semi-suite style accommodations, this residence hall is connected to advanced integrated multimedia lectern classrooms. Within the residential component, approximately 16,000 square feet features a fitness center, multiple social and/or gaming lounges, study lounges on each floor, laundry facilities, offices, and a tenth-floor sky lounge. The Academic & Residential Complex is LEED Gold certified by the US Green Building Council. Traditional units share community bathrooms and semi-suite style units provide an in-unit bathroom. Conveniently located near Student Center East, Taylor Street restaurants, and all east campus resources.")]
 
